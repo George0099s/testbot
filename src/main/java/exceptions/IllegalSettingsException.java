f0be0e71b990d5265204d38b5440e271c28a2881
Георгий Тер-Григорьянц <123456>
2021-08-02 16:09:56 +0300

    init

---
+++ src/main/java/exceptions/IllegalSettingsException.java
@@ -0,0 +1,11 @@
+package exceptions;
+
+/**
+ * Исключение, пробрасываемое в случае получения невалидных настроек выгружаемого файла
+ */
+public class IllegalSettingsException extends IllegalArgumentException {
+
+    public IllegalSettingsException(String s) {
+        super(s);
+    }
+}
